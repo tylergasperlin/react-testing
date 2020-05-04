6e119f35ea57a118828e5fa06d6a577502c51a07
Tyler Gasperlin <gasperlint@gmail.com>
2020-05-03 21:43:12 -0700

    Setup action

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,5 +1,11 @@
+import { SAVE_COMMENT } from 'action/types'
+import { saveComment } from '../actions';
+
+
 export default function (state = [], action) {
     switch (action.type) {
+        case SAVE_COMMENT:
+            return [...saveComment, action.payload]
         default: 
             return state;
     }
