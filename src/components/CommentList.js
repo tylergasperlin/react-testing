11323204d8b067c70ccd4c5fde48de86611da092
Tyler Gasperlin <gasperlint@gmail.com>
2020-05-04 05:57:28 -0700

    Render list of comments

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const CommentList = () => {
+    const comments = useSelector(state => state.comments)
+    
+    const renderComments = () => {
+        return comments.map(comment => {
+            return <li key={comment}> {comment} </li>
+        })
+    }
+
     return (
         <div>
-            Comment list
+            <ul>
+                {renderComments()}
+            </ul>
         </div>
     )
 }
