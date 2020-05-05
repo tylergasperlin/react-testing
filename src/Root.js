14e07b578220c0739ac4dfb68982bf6c5488ca71
Tyler Gasperlin <gasperlint@gmail.com>
2020-05-05 06:11:43 -0700

    Add fetch comments action creator

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers/index';
+import reduxPromise from 'redux-promise';
 
-export default ( {children, initialState} ) => {
-    return (
-        <Provider store={createStore(reducers, initialState)}>
-            {children}
-        </Provider>
-    )
-}
+export default ({ children, initialState }) => {
+    const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
+
+    return <Provider store={store}>{children}</Provider>;
+};
