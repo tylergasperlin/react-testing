aeff314f24a3fbbfd9a5231a107a91a9312e0850
Tyler Gasperlin <gasperlint@gmail.com>
2020-05-07 06:26:29 -0700

    Hook up moxios

--- src/actions/__tests__/integrations.tests.js
+++ src/actions/__tests__/integrations.tests.js
@@ -2,6 +2,19 @@ import React from 'react'
 import { mount } from 'enzyme';
 import Root from '../../Root';
 import App from '../../components/App'
+import moxios from 'moxios';
+
+beforeEach(() => {
+    moxios.install();
+    moxios.stubRequest('https:/jsonplaceholder.typicode.com/comments', {
+        status: 200,
+        response: [{name: 'Fetched #1'}, {name: 'Fetched #2'}]
+    })
+})
+
+afterEach(() => {
+    moxios.uninstall();
+})
 
 it('cant fetch a list of comments and display them', () => {
     // Render the app
@@ -13,5 +26,5 @@ it('cant fetch a list of comments and display them', () => {
     // Fetch comments
     wrapped.find('.fetch-comments').simulate('click')
     // expect to find a list of comments
-    expect(wrapped.find('li').length).toEqual(500);
+    expect(wrapped.find('li').length).toEqual(2);
 })
